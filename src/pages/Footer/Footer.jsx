f04787c4a3680a679d818964bbc9caaba7594e7d
SoraK93 <SoraK93@users.noreply.github.com>
2025-12-11 17:35:39 +0530

    Add Footer component

---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        This is part of a Project{" "}
+        <a href="https://github.com/SoraK93/reddit-client" target="_blank">
+          reddit-client
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export { Footer };

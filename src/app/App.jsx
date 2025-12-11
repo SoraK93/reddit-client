7985c2b905856ae22c32b88208f3bbf49f2c1448
SoraK93 <SoraK93@users.noreply.github.com>
2025-12-11 20:44:01 +0530

    Update app.jsx with posts and community component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import { Header } from "../pages/Header/Header";
+import { Footer } from "../pages/Footer/Footer";
+import { Posts } from "../pages/posts/Posts";
+import { Community } from "../pages/community/Community";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +11,11 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <Posts />
+        <Community />
+      </main>
+      <Footer />
     </>
   );
 }

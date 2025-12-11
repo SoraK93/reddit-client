a9517f9bddd40cbd9ab50a33bb124fbd21865a8e
SoraK93 <SoraK93@users.noreply.github.com>
2025-12-11 02:59:42 +0000

    Add page header component

---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Header = () => {
+  const [searchInput, setSearchInput] = useState();
+
+  return (
+    <>
+      <header>
+        <div>
+          <img src="#" alt="Reddit Logo" />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="search"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </div>
+        <div>
+          <img src="#" alt="User porfile image" />
+          <p>##UserName</p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export { Header };

2c5e82e29b2a2aebfd701d906c5b87f01f79255c
SoraK93 <SoraK93@users.noreply.github.com>
2025-12-11 02:59:42 +0000

    changes made to some placeholder code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Header } from "../pages/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

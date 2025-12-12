080eb27d7254e8c67335accc6c630664f62b8558
SoraK93 <SoraK93@users.noreply.github.com>
2025-12-12 09:36:23 +0530

    Add reddit api for fetching all posts

---
+++ src/api/allposts.js
@@ -0,0 +1,27 @@
+const fetchAPI = {
+  allPostLink: {
+    data: "/data/allposts.json",
+    url: "https://www.reddit.com/r/all.json",
+  },
+  post_comment: "/data/post_comment.json",
+  // https://www.reddit.com/r/FFXVI/comments/1pj09nu/can_we_talk_about_this_squareenix/{comments/.json}
+  user: "/data/user.json",
+  // https://www.reddit.com/user/userName{.json}
+  subreddit: "/data/subreddit.json",
+  // https://www.reddit.com/r/subreddit{.json}
+};
+
+const getAllPosts = async () => {
+  try {
+    const response = await fetch(fetchAPI.allPostLink.data);
+    // const response = await fetch(fetchAPI.allPostLink.url);
+    const data = await response.json();
+    const allPosts = data.data.children;
+    console.log(allPosts);
+    return allPosts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getAllPosts };

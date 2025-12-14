e60a51d1d3436bc5dca796739060bf35baaee8a9
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-14 16:22:50 +0530

    Reddit post initial layout

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../../api/allposts";
+import { render } from "@testing-library/react";
+
 const Posts = () => {
-  return;
+  const [newPost, setNewPost] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await getAllPosts();
+      setNewPost(...newPost, response);
+    };
+    fetchPost();
+  }, []);
+
+  const renderPostData = newPost.slice(0, 10);
+  console.log(renderPostData);
+  return (
+    <div>
+      {renderPostData.slice(0, 10).map((post) => {
+        return (
+          <article>
+            <div>
+              <div>
+                <p>{post.data.author}</p>
+                <p>r/{post.data.subreddit}</p>
+              </div>
+              <h2>{post.data.title}</h2>
+            </div>
+            <img src={post.data.url} alt="" />
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export { Posts };

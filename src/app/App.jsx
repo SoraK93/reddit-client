290b52b58a6e3d66008aeff37d96c55445aea28f
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-14 16:21:00 +0530

    Remove useState call inside App

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import { Header } from "../pages/Header/Header";
 import { Footer } from "../pages/Footer/Footer";
@@ -6,8 +5,6 @@ import { Posts } from "../pages/posts/Posts";
 import { Community } from "../pages/community/Community";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />

9e899d8434b78c0e8d8291a61055dc603e02b443
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-15 12:54:55 +0530

    Add Community component This renders all the community that have made recent post.

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -1,5 +1,27 @@
-const Community = () => {
-  return;
+import style from "./Community.module.css";
+
+const Community = ({ community }) => {
+  const communityList = [];
+
+  for (let item of community) {
+    communityList.push(
+      <li>
+        <img src={item.thumbnail.url} alt="" />
+        <p>{item.subreddit}</p>
+      </li>
+    );
+  }
+
+  return (
+    <section>
+      <div>
+        <h2>Recent Community Post</h2>
+      </div>
+      <div>
+        <ul>{communityList}</ul>
+      </div>
+    </section>
+  );
 };
 
 export { Community };

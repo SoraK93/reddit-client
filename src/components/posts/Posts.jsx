25f2773b7bb638b71c8f09a18a4e2d52fb05aa8f
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-16 18:32:35 +0530

    Add new parameter to Posts component and prop to SinglePost component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,13 +1,13 @@
 import { SinglePost } from "../SinglePost/SinglePost";
 import style from "./Post.module.css"
 
-const Posts = ({ newPost }) => {
+const Posts = ({ newPost, sub }) => {
   const renderPostData = newPost.slice(0, 10);
 
   return (
     <div>
       <ul>
-        <SinglePost renderPostData={renderPostData} />
+        <SinglePost renderPostData={renderPostData} sub={sub} />
       </ul>
     </div>
   );

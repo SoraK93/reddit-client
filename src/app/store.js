cff60d64b7eb8e3ce82b741d54a2550ab7c490cd
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-18 17:31:05 +0530

    update reducer object by adding post reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "../features/searchSlice/searchSlice";
+import postReducer from "../features/postSlice/postSlice"
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
+    post: postReducer,
   },
 });

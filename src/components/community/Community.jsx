d64fda52baa3a37c54ae08901618621e8fc212e2
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-18 17:30:00 +0530

    Replace forloop with map removed unnecessary code in order to make is more clean and readable

--- src/components/community/Community.jsx
+++ src/components/community/Community.jsx
@@ -1,24 +1,22 @@
 import style from "./Community.module.css";
 
 const Community = ({ community }) => {
-  const communityList = [];
-
-  for (let item of community) {
-    communityList.push(
-      <li>
-        <img src={item.thumbnail.url} alt="" />
-        <p>{item.subreddit}</p>
-      </li>
-    );
-  }
-
   return (
     <section>
       <div>
         <h2>Recent Community Post</h2>
       </div>
       <div>
-        <ul>{communityList}</ul>
+        <ul>
+          {community.map((item) => {
+            return (
+              <li key={item.id}>
+                <img src={item.thumbnail.url} alt="" />
+                <p>{item.name_prefix}</p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );

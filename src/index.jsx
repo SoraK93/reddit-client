c9427816ace60a598f5e7d5c9c5c892ab37d1d04
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-19 17:39:57 +0530

    Add route functionality to our project

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app/App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import routes from "./app/routes.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );

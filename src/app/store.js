5d58ae2831d02baa8093e679e3acc0935cfb6216
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-12-19 17:39:11 +0530

    Import removed - searchReducer no longer exists

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import searchReducer from "../features/searchSlice/searchSlice";
 import postReducer from "../features/posts/postSlice/postSlice"
 
 export const store = configureStore({

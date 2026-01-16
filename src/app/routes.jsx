7d21249904730783bfab0010c9fdda879dc88c67
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-01-16 07:30:15 +0530

    Refactor our path structure, now using children path too After considering whats being rendered when, its unnecessary to render community each time. So, we are going to use outlet to make it much simpler and reduce rendering of unnecessary components.

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,8 +1,19 @@
-import { Posts } from "../features/posts/Posts";
+import { AllPost } from "../features/posts/components/AllPost";
 import App from "./App";
+import { SinglePost } from "../features/posts/components/single";
 
 export default [
-  { path: "/", element: <App /> },
-  { path: "/r/:subreddit", element: <App />},
-  { path: "/r/:subreddit/comments/:id", element: <App />}
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <AllPost /> }],
+  },
+  {
+    path: "r/:subreddit",
+    element: <App />,
+    children: [
+      { index: true, element: <AllPost /> },
+      { path: "comments/:id", element: <SinglePost /> },
+    ],
+  },
 ];

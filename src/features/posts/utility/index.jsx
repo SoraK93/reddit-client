aae1ca16f0c60a8bf9ab3ca98976d834bfcf81f7
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-01-16 07:41:04 +0530

    Add helper functions Will be modified later, if a certain use case arises. My target is to have flexible helper functions which can be used in multiple ways.

---
+++ src/features/posts/utility/index.jsx
@@ -0,0 +1,56 @@
+const getPostAttachment = (data, mediaType) => {
+  switch (mediaType) {
+    case "image":
+      return <img src={data.url} alt="#" />;
+    case "hosted:video":
+      let source = data.secure_media?.reddit_video;
+      return (
+        <video
+          width={source.width}
+          height={source.height}
+          autoPlay
+          muted
+          controls
+        >
+          <source src={source.hls_url} type="video/mp4" />
+        </video>
+      );
+    case "link":
+      return <a href={data.url}>{data.url}</a>;
+  }
+};
+
+const getSubredditThumbnail = (data, subreddit) => {
+  if (data.subreddit_name_prefixed === subreddit.name_prefix) {
+    return <img src={subreddit.thumbnail.url} alt="" />;
+  }
+};
+
+const createCommentList = (comments) => {
+  const list = [];
+
+  for (let comment of comments) {
+    const data = comment.data;
+
+    const currentComment = {
+      author: data.author,
+      comment: data.body,
+      replies: null,
+    };
+
+    if (data.replies === "") {
+      list.push(currentComment);
+      continue;
+    } else {
+      const repliesList = data.replies.data.children.filter(
+        (reply) => reply.kind !== "more"
+      );
+      currentComment.replies = createCommentList(repliesList);
+      list.push(currentComment);
+    }
+  }
+
+  return list.length > 0 ? list : null;
+};
+
+export { getPostAttachment, getSubredditThumbnail, createCommentList };

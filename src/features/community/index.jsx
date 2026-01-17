8135cfa45f5292f1e41cbbef174b0e053c03b1da
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-01-17 06:03:32 +0530

    Removed unnecessary imports

--- src/features/community/index.jsx
+++ src/features/community/index.jsx
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import style from "./Community.module.css";
 import { selectSubreddit } from "../posts/postSlice/postSlice";
-import { fetchSubredditPost } from "../../api/redditAPI";
 import { useNavigate } from "react-router";
 
 const Community = () => {
